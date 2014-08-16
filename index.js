3017cceaa62b01cb86dda8d2b550d0bcf23e2c41
olivierrr <oliviersinbox@gmail.com>
2014-08-16 04:18:22 -0700

    gradient helper?

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var WAVEFORM = function(options) {
 	this.container = options.container
 	this.height = options.height || 100
 	this.width = options.width || 300
-	this.waveform = options.waveform 
+	this.waveform = options.waveform
+
+	this.colors = {}
 
 	this.gutter = options.gutter || 1
 	this.barWidth = options.barWidth || 3
@@ -25,11 +27,14 @@ WAVEFORM.prototype.init = function() {
 	// append canvas
 	this.container.appendChild(this.canvas)
 
+	this.color('gutter', ['#6B6B6B', 0, '#c9c9c9', 1])
+	this.color('bar', ['#666666', 0, '#868686', 1])
+
 	//render
 	this.draw()
 
 }
-
+////bar-bottom #666666  //bar-top #868686 //gutter-bottom #6B6B6B //gutter-top #909090
 WAVEFORM.prototype.update = function(options) {
 
 	console.log('update')
@@ -66,6 +71,20 @@ WAVEFORM.prototype.update = function(options) {
 	this.draw()
 }
 
+// 'experimental'
+WAVEFORM.prototype.color = function(name, colors) {
+
+	var gradient = this.ctx.createLinearGradient(0,100,0,0)
+
+	for(var i=0; i<colors.length; i+=2) {
+		gradient.addColorStop(colors[i+1], colors[i])
+	}
+
+	this.colors[name] = gradient
+
+	this.draw()
+};
+
 WAVEFORM.prototype.draw = function() {
 
 	console.log('draw')
@@ -75,22 +94,17 @@ WAVEFORM.prototype.draw = function() {
 	var xPos = 0
 	var yPos = 100
 
-	//should be on init // should be updatedable
-	gradient = this.ctx.createLinearGradient(0,100,0,0)
-	gradient.addColorStop(0, '#373737')
-	gradient.addColorStop(1, '#BDBDBD')
-
 	//clear canvas for redraw
 	this.ctx.clearRect ( 0 , 0 , this.width , this.height );
 	
 	for(var i=0; i<waves.length; i+=1) {
 
 		// main bar
-		this.ctx.fillStyle = '#333333'
+		this.ctx.fillStyle = this.colors['bar']
 		this.ctx.fillRect(xPos, yPos, this.barWidth, Math.floor(-Math.abs(waves[i]*100)))
 
 		// gutter
-		this.ctx.fillStyle = gradient
+		this.ctx.fillStyle = this.colors['gutter']
 		var smaller = Math.min(waves[i],waves[i+1])
 		this.ctx.fillRect(xPos + this.barWidth, yPos, this.gutter, Math.floor(-Math.abs(smaller*100)))
 		
@@ -103,6 +117,8 @@ WAVEFORM.prototype.draw = function() {
 
 }
 
+
+// need more accurate algo
 WAVEFORM.prototype.genWaves = function() {
 
 	console.log('genWaves')
@@ -110,8 +126,6 @@ WAVEFORM.prototype.genWaves = function() {
 	var result, waves, wave, i
 
 	var lines = (this.width / (this.gutter + this.barWidth) )
-
-	//var lines = (this.width - )
 
 	result = Math.round(this.waveform.length / lines)
 
